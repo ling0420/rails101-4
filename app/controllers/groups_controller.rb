89b8155ac6c9710038303d55d057dce1e832a945
Leo <17959547@qq.com>
2017-09-13 16:24:43 +0800

    join & quit group action, when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :destroy, :update]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :destroy, :update, :join, :quit]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -37,11 +37,37 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
-
+      current_user.join!(@group)
       redirect_to groups_path
     else
       render :new
     end
+  end
+
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本座成功"
+    else
+      flash[:warning] = "你已经是本座成员"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本座"
+    else
+      flash[:warning] = "你不是本座成员，怎么退出 xd"
+    end
+
+    redirect_to group_path(@group)
   end
 
     private
