49248716a8c77985a998b2456a94239154a2e22a
Leo <17959547@qq.com>
2017-09-13 14:38:15 +0800

    add post and hook group/user relation, add posts nested route

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :destroy, :update]
-  before_action :find_group_and_check_permission, only: {:edit, :update, :destroy}
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -51,7 +51,6 @@ class GroupsController < ApplicationController
       if current_user != @group.user
         redirect_to root_path, alert: "You have no permission"
       end
-
     end
 
     def group_params
